16d975e8436d6d0e92c421ab47d84c13ef5c253e
Raj <rmahaja1@stevens.edu>
2021-05-13 23:29:23 -0400

    Added description

--- memestonks/src/components/Home.js
+++ memestonks/src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import SignOut from './SignOut';
 import {
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 function Root() {
     return (
         <div>
@@ -10,13 +10,30 @@ function Root() {
             <div>
                 <Link to="/stock-chart">Click here for real time stock prices</Link>
             </div>
+            <br />
             <div>
                 <Link to="/signin">Click here to Sign in!</Link>
             </div>
+            <br />
             <div>
                 <Link to="/signup">Click here to Sign Up!</Link>
             </div>
-<SignOut/>
+            <br />
+            <div>
+                <br />
+                <br />
+                <div className="description">
+                    <p> A meme stock is a stock that has seen an increase in volume not because of the
+                    company’s performance, but rather because of hype on social media and online forums like Reddit. </p>
+
+                    <p> Examples of meme stocks include GameStop, AMC, and BlackBerry. While the companies themselves have not performed well in recent years,
+                    all three stocks went viral on a popular Reddit forum and saw massive price increases in
+                    late January 2021, specifically Jan. 27. BlackBerry’s stock more than tripled, while AMC increased by nearly tenfold.
+                </p>
+                </div>
+            </div>
+
+            <SignOut />
         </div>
     );
 }
