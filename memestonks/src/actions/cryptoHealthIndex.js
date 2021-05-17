5303a35de4fcf25633b24c591c9a0bbc7757dc50
vineethvs23 <vinetthvs2301@gmail.com>
2021-05-17 08:58:45 -0400

    Added Crypto health action and reducers

---
+++ memestonks/src/actions/cryptoHealthIndex.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+export const getCryptoHealth = (symbol) => async (dispatch) => {
+  const API_KEY = "9SEJV46K465GDV17";
+  let ticker = symbol;
+
+  try {
+    let cryptoHealth = await axios(
+      `http://localhost:8080/cryptoHealthIndex/${ticker}`
+    );
+    console.log(cryptoHealth.data);
+    dispatch({
+      type: "GET_CRYPTO_HEALTH",
+      payload: cryptoHealth.data,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
--- memestonks/src/reducers/rootReducer.js
+++ memestonks/src/reducers/rootReducer.js
@@ -4,10 +4,12 @@ import financialItemWeekly from "./financialItemWeeklyReducer";
 import financialItemMonthly from "./financialItemMonthlyReducer";
 import financialOverview from "./financialOverviewReducer";
 import cryptoExchange from "./cryptoExchangeReducer";
+import cryptoHealth from "./cryptoHealthReducer";
 export default combineReducers({
   financialItem,
   financialItemWeekly,
   financialItemMonthly,
   financialOverview,
   cryptoExchange,
+  cryptoHealth,
 });
