e8f4794fac6133ad2accdcbd6c63704474021d99
vineethvs23 <vinetthvs2301@gmail.com>
2021-05-17 18:31:45 -0400

    Added Crypto weekly reducer

---
+++ memestonks/src/reducers/cryptoWeeklyReducer.js
@@ -0,0 +1,19 @@
+const initialState = {
+  financialItem: null,
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "GET_CRYPTO_WEEKLY":
+      console.log("payload", payload);
+      return {
+        ...state,
+        financialItem: payload,
+      };
+
+    default:
+      return state;
+  }
+}
