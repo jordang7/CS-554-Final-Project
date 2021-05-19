3b0c9ff6c2919790d64aa1a72049010660f42956
jordang7 <77jgreenberg77@gmail.com>
2021-05-19 16:59:54 -0400

    about us content

--- memestonks/src/components/aboutus.js
+++ memestonks/src/components/aboutus.js
@@ -4,7 +4,30 @@ import '../css/home.css'
 function Root() {
     return (
         <div>
-            <h1> About us 🌕🚀</h1>            
+            <h1> About us 🌕🚀</h1>
+            <p>
+                We are Students at Stevens Institute of Technology, and this is our final project for CS554(Web development II) with Professor Patrick Hill
+            </p>
+            <h2 className="title">
+                Our Team </h2>
+                <ul>
+                    <li>
+                        <spa></spa>
+                    </li>
+                    <li>
+                        Jordan Greenberg
+                    </li>
+                    <li>
+                        Vineeth VS
+                    </li>
+                    <li>
+                    Raj Mahajan
+                    </li>
+                    <li>
+                    Himanshu Bishnoi
+                    </li>
+                </ul>
+
         </div>
     );
 }
